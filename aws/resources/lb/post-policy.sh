4208eeae2306f0cf4c8282b54a4604e2e7006f42
J. Hunter Hawke <j.hunter.hawke@gmail.com>
2022-03-24 14:51:25 +0100

    Fixed a bad path for CRDs

--- aws/resources/lb/post-policy.sh
+++ aws/resources/lb/post-policy.sh
@@ -4,6 +4,6 @@ test -n "$3" && echo ACCOUNT is "$3" || "echo ACCOUNT is not set && exit"
 test -n "$LBC_VERSION" && echo LBC_VERSION is "$LBC_VERSION" || "export LBC_VERSION=2.1.0"
 helm repo add eks https://aws.github.io/eks-charts
 # create the custom resource definition for the load balancer
-kubectl apply -f resources/lb/crds.yaml
+kubectl apply -f crds.yaml
 # install the load balancer controller using the helm chart 
 helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=$2 --set serviceAccount.name=aws-load-balancer-controller --set image.repository=602401143452.dkr.ecr.$1.amazonaws.com/amazon/aws-load-balancer-controller --set image.tag="v2.3.0"
